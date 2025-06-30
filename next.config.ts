b55c510c429327c65473c0e0286716464fb5a7fb
Samminga Sainath Rao <sammingasainathrao@gmail.com>
2025-06-30 10:19:20 +0530

    deployment errors fix 1

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/portfolio",
+  // TODO: Update basePath if your repository name is not "portfolio"
 };
 
 export default nextConfig;
